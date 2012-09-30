233bf3d71898a68217d2b566a451481c43fad3bb
jig <jig@scryptor>
2012-09-30 15:49:26 +0200

    client now supports concurrency
    client now supports concurrency with proper socket reusage

--- client.js
+++ client.js
@@ -8,11 +8,15 @@ args
 	.version('0.1')
 	.option('-p, --port [number]', 'server port', 80)
 	.option('-s, --server [number]', 'server address', 'localhost')
-	.option('-1, --arg1 [number]', 0)
-	.option('-2, --arg2 [number]', 0)
-	.option('-r, --rawreq [data]', '{}')
-	.option('-o, --operation [op_name]', 'addition')
+	.option('-1, --arg1 [number]', 'first argument', 0)
+	.option('-2, --arg2 [number]', 'second argument', 0)
+	.option('-r, --rawreq [data]', 'request in JSON sintax', '{}')
+	.option('-o, --operation [op_name]', 'operation identifier', 'addition')
+	.option('-n, --number [n]', 'number of concurrent iterations', 1)
+	.option('-c, --concurrent [n]', 'maximum concurrency', 1)
 	.parse(process.argv);
+
+http.globalAgent.maxSockets = parseInt(args.concurrent, 10);
 
 if(args.rawreq) {
 	var data = args.rawreq;
@@ -23,52 +27,50 @@ if(args.rawreq) {
 					]);
 }
 
-var options = {}
+var options = {
+			host: args.server,
+			port: 80,
+			method: 'POST',
+			path: '/' + args.operation,
+			headers: {
+				'Host': args.server,
+				'Content-Length': data.length,
+				'Content-Type': 'application/json'
+			}
+		}
 
+var socketName = args.server + ':' + args.port;
+		
 if(args.proxy) {
 	var regex = '([^:]+):([0-9]+)';
-	options = {
-		host: args.proxy.match(regex)[1],
-		port: args.proxy.match(regex)[2],
-		method: 'POST',
-		path: 'http://' + args.proxy + '/' + args.operation,
-		headers: {
-			'Host': args.server,
-			'Content-Length': data.length,
-			'Content-Type': 'application/json'
-		}
-	}
-} else {
-	options = {
-		host: args.server,
-		port: 80,
-		method: 'POST',
-		path: '/' + args.operation,
-		headers: {
-			'Host': args.server,
-			'Content-Length': data.length,
-			'Content-Type': 'application/json'
-		}
-	}
+	options.host = args.proxy.match(regex)[1];
+	options.port = args.proxy.match(regex)[2];
+	options.path = 'http://' + args.proxy + '/' + args.operation;
 }
 
-var request = http.request(
-					options, 
-					function(response){
-						var resData = '';
+var chainRequest = function (options, n) {
+	var request = http.request(
+						options, 
+						function(response){
+							var resData = '';
+							response.on('data', function (chunk) {
+								resData += chunk;
+							});
+							response.on('end', function () {});
 
-						response.on('data', function (chunk) {
-							resData += chunk;
+							if(n>0) {
+								chainRequest(options, n-1);
+							}
 						});
-						response.on('end', function () {
-							console.log("Result: " + resData);
-						});
-					});
 
-// Tractament d'errors.
-request.on('error', function (error) {
-	console.log("ERROR: " + error.message);
-});
+	request.on('error', function (error) {
+		console.log("ERROR: " + error.message);
+	});
 
-request.write(data);
-request.end();
+	request.write(data);
+	request.end();
+}
+
+for(var i=0; i<args.concurrent; ++i) {
+	chainRequest(options, args.number);
+}
